e484a31e0582bc3f541bb536bcd180bf2938e4a3
Javi <realorth@gmail.com>
2020-01-22 08:27:51 +0100

    pagina terminada final

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 						<div class="content">
 							<div class="inner">
 								<h1>Javier García Gil</h1>
-								<p>Holaaa!! Me llamo Javier y estudio en <a href="http://decroly.com/">DECROLY</a> un grado Superior "Administrador de Sistemas Informaticos en Red</p>
+								<p>Holaaa!! Me llamo Javier y estudio en <a href="http://decroly.com/">DECROLY</a> un grado Superior "Administrador de Sistemas Informaticos en Red"</p>
 							</div>
 						</div>
 						<nav>
@@ -28,7 +28,7 @@
 								<li><a href="#intro">Redes Sociales</a></li>
 								<li><a href="#work">GitHub</a></li>
 								<li><a href="#about">StackOverflow</a></li>
-								<li><a href="#contact">Contact</a></li>
+								<li><a href="#contact">Formulario</a></li>
 								<!--<li><a href="#elements">Elements</a></li>-->
 							</ul>
 						</nav>
@@ -40,33 +40,32 @@
 						<!-- Intro -->
 							<article id="intro">
 								<h2 class="major">Redes Sociales</h2>
-								<span class="image main"><img src="redes.png" alt="" /></span>
+								<span class="image main"><img src="images/redes.png" alt="" /></span>
 								<p><a href="https://www.instagram.com/gargiljavi/">Instagram</a>.</p>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
 							</article>
 
 						<!-- Work -->
 							<article id="work">
-								<h2 class="major">Work</h2>
-								<span class="image main"><img src="images/pic02.jpg" alt="" /></span>
-								<p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-								<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+								<h2 class="major">GitHub</h2>
+								<span class="image main"><img src="images/G.png" alt="" /></span>
+								<p><a href="https://github.com/gargiljavi/asir1_javi">Repositorio ASIR1 GitHub</a>.</p>
+
 							</article>
 
 						<!-- About -->
 							<article id="about">
-								<h2 class="major">About</h2>
-								<span class="image main"><img src="images/pic03.jpg" alt="" /></span>
-								<p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+								<h2 class="major">StackOverflow</h2>
+								<span class="image main"><img src="images/1.png" alt="" /></span>
+								<p><a href="https://es.stackoverflow.com/users/141991/javier-garc%c3%ada-gil">StackOverflow</a>.</p>
 							</article>
 
 						<!-- Contact -->
 							<article id="contact">
-								<h2 class="major">Contact</h2>
+								<h2 class="major">Formulario</h2>
 								<form method="post" action="#">
 									<div class="fields">
 										<div class="field half">
-											<label for="name">Name</label>
+											<label for="name">Nombre</label>
 											<input type="text" name="name" id="name" />
 										</div>
 										<div class="field half">
@@ -74,22 +73,16 @@
 											<input type="text" name="email" id="email" />
 										</div>
 										<div class="field">
-											<label for="message">Message</label>
+											<label for="message">Deja tu mensaje</label>
 											<textarea name="message" id="message" rows="4"></textarea>
 										</div>
 									</div>
 									<ul class="actions">
-										<li><input type="submit" value="Send Message" class="primary" /></li>
-										<li><input type="reset" value="Reset" /></li>
+										<li><input type="submit" value="Enviar" class="primary" /></li>
+										<li><input type="reset" value="Borrar" /></li>
 									</ul>
 								</form>
-								<ul class="icons">
-									<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-									<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-									<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-									<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-								</ul>
-							</article>
+															</article>
 
 						<!-- Elements -->
 							<article id="elements">
